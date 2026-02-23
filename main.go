3e5ba087726b0c693fa659b978f8ace1aa6167d8
evecus <jamie55@163.com>
2026-02-23 22:22:02 +0800

    Update main.go

--- main.go
+++ main.go
@@ -39,7 +39,6 @@ func main() {
 	}
 
 	fmt.Printf("\n🚀 EasyPanel running on http://0.0.0.0:%d\n", config.Main.Port)
-	fmt.Printf("   Default credentials: admin / admin\n\n")
 
 	r := gin.Default()
 
